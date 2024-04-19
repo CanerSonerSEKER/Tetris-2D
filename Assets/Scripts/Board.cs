c8769d325a84e9cab76d52c20088fd5019f97bb4
Caner Soner ŞEKER <110617690+CanerSonerSEKER@users.noreply.github.com>
2024-04-19 22:21:20 +0300

    Keyboard Movement Completed.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,4 @@
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
@@ -9,6 +8,16 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; }
     public TetrominoData[] _tetrominoes;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -39,8 +48,38 @@ public class Board : MonoBehaviour
     {
         for (int i = 0; i < piece.cells.Length; i++)
         {
-            Vector3Int tilePosition = piece.cells[i] + piece.position;
-            this._tilemap.SetTile(tilePosition, piece.data._tile);
+            Vector3Int tilePosition = piece.cells[i] + piece._position;
+            this._tilemap.SetTile(tilePosition, piece._data._tile);
         }
     }
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece._position;
+            this._tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+        
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+            if (this._tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,27 +1,78 @@
+using System.Net;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    public Board board { get; private set; }
-    public TetrominoData data { get; private set; }
+    public Board _board { get; private set; }
+    public TetrominoData _data { get; private set; }
     public Vector3Int[] cells { get; private set; } 
-    public Vector3Int position { get; private set; }
+    public Vector3Int _position { get; private set; }
     
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-        this.board = board;
-        this.data = data;
-        this.position = position;
+        this._board = board;
+        this._data = data;
+        this._position = position;
 
         if (this.cells == null)
         {
-            this.cells = new Vector3Int[data.cells.Length];
+            this.cells = new Vector3Int[_data.cells.Length];
         }
 
-        for (int i = 0; i < data.cells.Length; i++)
+        for (int i = 0; i < cells.Length; i++)
         {
-            this.cells[i] =(Vector3Int)data.cells[i];
+            this.cells[i] = (Vector3Int)_data.cells[i];
         }
         
-    }    
+    }
+
+    private void Update()
+    {
+        this._board.Clear(this);
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+        
+        this._board.Set(this);
+    }
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+        
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this._position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this._board.IsValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this._position = newPosition; 
+        }
+
+        return valid;
+    }
 }
